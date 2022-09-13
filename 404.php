c4a0be997ffd72ecd06c5853b3b4a80c9c1b78ce
Camila Cordeiro da Rocha <camilaa7x@gmail.com>
2022-09-13 16:45:19 -0300

    criacao home, index,  login, menu categoria

---
+++ 404.php
@@ -0,0 +1,1 @@
+Erro! Página Não Encontrada.
---
+++ login.php
@@ -0,0 +1,9 @@
+<form class="form-control" method="post">
+    <label for="login" class="form-label">Login:</label>
+    <input class="form-control" type="text" name="login" id="login" placeholder="e-mail">
+    <br>
+    <label for="senha" class="form-label">Senha:</label>
+    <input class="form-control" type="password" name="senha" id="senha">
+    <br>
+    <input class="form-control" type="submit" name="autenticar" value="Logar">
+</form>
